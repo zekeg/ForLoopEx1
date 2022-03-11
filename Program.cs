3dcda296b72db207f9c233411bb2363de24c7427
Zechariah Guadiana <zechariah.guadiana@gmail.com>
2022-03-10 18:34:50 -0600

    Initial commit for muscle memory for loop exercise

---
+++ Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace ForLoopEx1
+{
+    internal class ForLoopEx1
+    {
+
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"{i}");
+            }
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"{i}");
+            }
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"{i}");
+            }
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"{i}");
+            }
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"{i}");
+            }
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"{i}");
+            }
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"{i}");
+            }
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"{i}");
+            }
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"{i}");
+            }
+            for (int i = 0; i < 5; i++) {
+                Console.WriteLine($"{i}");
+            }
+            for (int i = 0; i < 5; i++) {
+                Console.WriteLine($"{i}");
+            }
+            for (int i = 0; i < 5; i++) {
+                Console.WriteLine($"{i}");
+            }
+            for (int i = 0; i < 5; i++) {
+                Console.WriteLine($"{i}");
+            }
+            for (int i = 0; i < 5; i++) {
+                Console.WriteLine($"{i}");
+            }
+        }
+    }
+}
